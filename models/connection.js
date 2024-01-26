4ce6d13b5033cde1de6870a44911b726fcecfbea
Emilien <emiliengir@gmail.com>
2024-01-26 16:41:16 +0100

    test

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://EmilienJy:MVwAAIC5O6hySBov@cluster1.6v3kauk.mongodb.net/weather";
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
