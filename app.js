fc74ba9a3a987ee4d872c652e68b075571591eb8
Emilien <emiliengir@gmail.com>
2024-01-27 14:34:22 +0100

    ajout cors test

--- app.js
+++ app.js
@@ -12,9 +12,18 @@ var indexRouter = require('./routes/index');
 var weatherRouter = require('./routes/weather');
 var userRouter = require('./routes/users');
 
+// Autoriser uniquement les requêtes depuis votre domaine front-end
+const corsOptions = {
+    origin: 'https://weatherapp-frontend-sigma-henna.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
 var app = express();
 const cors = require('cors');
 app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,5 +34,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/weather', weatherRouter);
 app.use('/users', userRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
